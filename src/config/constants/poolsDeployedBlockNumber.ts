093ecf82ac7ab0554c4c291de0ca2750a3cf0649
Eugeniy Zaychenko <ezaychenko@codyua.com>
2022-04-17 14:19:23 +0300

    Update tests

--- src/config/constants/poolsDeployedBlockNumber.ts
+++ src/config/constants/poolsDeployedBlockNumber.ts
@@ -3,11 +3,11 @@ import { CURRENT_CHAIN_ID } from 'config'
 import { PoolDeployedBlockNumber } from './types'
 
 const poolsDeployedBlockNumberMAINNET: PoolDeployedBlockNumber = {
-  '0x35f64b2f6824d8Be046Ac854c5E4db72f6D9cE79': 18467024, // Update with MAINNET EVT
+  '0x2061037E588b66Ef8379E1E5216D10bb26e07106': 18467024, // Update with MAINNET EVT
 }
 
 const poolsDeployedBlockNumberTESTNET: PoolDeployedBlockNumber = {
-  '0x35f64b2f6824d8Be046Ac854c5E4db72f6D9cE79': 18467024, // Update with TESTNET EVT
+  '0x2061037E588b66Ef8379E1E5216D10bb26e07106': 18467024, // Update with TESTNET EVT
 }
 
 const BY_NET = {
