c3f5dd70570dd1038c31100bda062c7fe65eafcf
Alexey Kerpel <alexeykerpel15@gmail.com>
2022-04-19 15:44:20 +0300

    Added redirect from home page

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,75 +1,82 @@
 import React from 'react'
-import { Flex } from '@envoysvision/uikit'
-import styled from 'styled-components'
-import PageSection from 'components/PageSection'
-import { useWeb3React } from '@web3-react/core'
-import useTheme from 'hooks/useTheme'
-import Container from 'components/Layout/Container'
-import { PageMeta } from 'components/Layout/Page'
-import Hero from './components/Hero'
-import { swapSectionData, earnSectionData, cakeSectionData } from './components/SalesSection/data'
-import MetricsSection from './components/MetricsSection'
-import SalesSection from './components/SalesSection'
-import WinSection from './components/WinSection'
-import FarmsPoolsRow from './components/FarmsPoolsRow'
-import Footer from './components/Footer'
-import CakeDataRow from './components/CakeDataRow'
-import { WedgeTopLeft, InnerWedgeWrapper, OuterWedgeWrapper, WedgeTopRight } from './components/WedgeSvgs'
-import UserBanner from './components/UserBanner'
-import FarmAuctionsBanner from './components/Banners/FarmAuctionsBanner'
+import { useRouter } from 'next/router'
+// import { Flex } from '@envoysvision/uikit'
+// import styled from 'styled-components'
+// import PageSection from 'components/PageSection'
+// import { useWeb3React } from '@web3-react/core'
+// import useTheme from 'hooks/useTheme'
+// import Container from 'components/Layout/Container'
+// import { PageMeta } from 'components/Layout/Page'
+// import Hero from './components/Hero'
+// import { swapSectionData, earnSectionData, cakeSectionData } from './components/SalesSection/data'
+// import MetricsSection from './components/MetricsSection'
+// import SalesSection from './components/SalesSection'
+// import WinSection from './components/WinSection'
+// import FarmsPoolsRow from './components/FarmsPoolsRow'
+// import Footer from './components/Footer'
+// import CakeDataRow from './components/CakeDataRow'
+// import { WedgeTopLeft, InnerWedgeWrapper, OuterWedgeWrapper, WedgeTopRight } from './components/WedgeSvgs'
+// import UserBanner from './components/UserBanner'
+// import FarmAuctionsBanner from './components/Banners/FarmAuctionsBanner'
 
-const showBanner = false
+// const showBanner = false
 
-const HomeBanner = ({ account }: { account: string }) => {
-  if (!showBanner) {
-    return null
-  }
+// const HomeBanner = ({ account }: { account: string }) => {
+//   if (!showBanner) {
+//     return null
+//   }
 
-  return (
-    <Flex
-      pt={[account ? '220px' : '0', null, null, account ? '76px' : '0']}
-      mt={[account ? '0' : '-16px', null, null, account ? '0' : '-48px']}
-      pb="24px"
-    >
-      <FarmAuctionsBanner />
-    </Flex>
-  )
-}
+//   return (
+//     <Flex
+//       pt={[account ? '220px' : '0', null, null, account ? '76px' : '0']}
+//       mt={[account ? '0' : '-16px', null, null, account ? '0' : '-48px']}
+//       pb="24px"
+//     >
+//       <FarmAuctionsBanner />
+//     </Flex>
+//   )
+// }
 
-const StyledHeroSection = styled(PageSection)`
-  padding-top: 16px;
+// const StyledHeroSection = styled(PageSection)`
+//   padding-top: 16px;
 
-  ${({ theme }) => theme.mediaQueries.md} {
-    padding-top: 48px;
-  }
-`
+//   ${({ theme }) => theme.mediaQueries.md} {
+//     padding-top: 48px;
+//   }
+// `
 
-const UserBannerWrapper = styled(Container)`
-  z-index: 1;
-  position: absolute;
-  width: 100%;
-  top: 0;
-  left: 50%;
-  transform: translate(-50%, 0);
-  padding-left: 0px;
-  padding-right: 0px;
+// const UserBannerWrapper = styled(Container)`
+//   z-index: 1;
+//   position: absolute;
+//   width: 100%;
+//   top: 0;
+//   left: 50%;
+//   transform: translate(-50%, 0);
+//   padding-left: 0px;
+//   padding-right: 0px;
 
-  ${({ theme }) => theme.mediaQueries.lg} {
-    padding-left: 24px;
-    padding-right: 24px;
-  }
-`
+//   ${({ theme }) => theme.mediaQueries.lg} {
+//     padding-left: 24px;
+//     padding-right: 24px;
+//   }
+// `
 
-const Home: React.FC = () => {
-  const { theme } = useTheme()
-  const { account } = useWeb3React()
+const Home = () => {
+  const router = useRouter()
 
-  const HomeSectionContainerStyles = { margin: '0', width: '100%', maxWidth: '968px' }
+  router.push('/swap')
 
-  return (
+  return null
+  // const { theme } = useTheme()
+  // const { account } = useWeb3React()
+
+  // const HomeSectionContainerStyles = { margin: '0', width: '100%', maxWidth: '968px' }
+
+  {
+    /* return (
     <>
       <PageMeta />
-      {/* <StyledHeroSection
+      <StyledHeroSection
         innerProps={{ style: { margin: '0', width: '100%' } }}
         background={
           theme.isDark
@@ -154,9 +161,10 @@ const Home: React.FC = () => {
         hasCurvedDivider={false}
       >
         <Footer />
-      </PageSection> */}
+      </PageSection>
     </>
-  )
+  ) */
+  }
 }
 
 export default Home
