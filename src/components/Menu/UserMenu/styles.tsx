8e2d1b2157443f66b03b776f33d809f5f725fe36
Anton Shyltsev <icanton.shyltsev@americanwell.com>
2022-05-12 22:17:49 +0300

    add connect btn animation

---
+++ src/components/Menu/UserMenu/styles.tsx
@@ -0,0 +1,83 @@
+import styled, { css } from 'styled-components'
+import { UserMenu } from '@envoysvision/uikit'
+
+export const UserMenuWrapper = styled.div`
+  position: relative;
+  border-radius: ${({ theme }) => theme.radii.default};
+  background: ${({ theme }) => theme.colors.background};
+`
+
+export const UserMenuContentWrapper = styled.div<{
+  withAccount?: boolean
+  isConnecting?: boolean
+  isDisconnecting?: boolean
+}>`
+  position: relative;
+`
+
+export const AccountWrapper = styled(UserMenu)<{ isConnecting?: boolean; isDisconnecting?: boolean }>`
+  transition: opacity ${({ theme }) => theme.animations.durationConnect} ease-in-out;
+  opacity: 0;
+  position: relative;
+
+  > div:first-child {
+    position: relative;
+    justify-content: space-between;
+    width: 100%;
+    margin: 0;
+    background: transparent;
+    padding-right: 12px;
+    > div {
+      display: flex;
+      align-items: center;
+      &:first-child {
+        width: 100%;
+      }
+    }
+  }
+
+  ${({ isConnecting, account }) =>
+    !account &&
+    isConnecting &&
+    css`
+      opacity: 1;
+      > div:first-child {
+        > div:last-child {
+          opacity: 1;
+        }
+      }
+    `}
+
+  ${({ account, isDisconnecting }) =>
+    account &&
+    !isDisconnecting &&
+    css`
+      transition: none;
+      opacity: 1;
+      > div:first-child {
+        > div:last-child {
+          opacity: 1;
+        }
+      }
+    `}
+
+  ${({ account, isDisconnecting }) =>
+    account &&
+    isDisconnecting &&
+    css`
+      opacity: 0;
+      > div:first-child {
+        > div:last-child {
+          width: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          > div {
+            &:first-child {
+              width: auto;
+            }
+          }
+        }
+      }
+    `}
+`
