2b1b77f3fa569723bfa85699ce82f6946aaf72ee
Sergey <dev.zaycev@gmail.com>
2022-08-13 17:14:01 +0300

    chore: implements airdrop details

--- src/views/Companies/components/Subpages/Airdrop/components/AirdropTokenDetails.tsx
+++ src/views/Companies/components/Subpages/Airdrop/components/AirdropTokenDetails.tsx
@@ -1,33 +1,34 @@
 import React from 'react'
 import { Link } from '@envoysvision/uikit'
 import TextWithHeader from '../../components/TextWithHeader'
-import { BaseCompany } from '../../../../utils'
+import { Token } from '@envoysvision/sdk'
+import { EnvoysAirdrop } from 'config/abi/types'
+import useTotalSupply from 'hooks/useTotalSupply'
 
-const AirdropTokenDetails: React.FC<{ company: BaseCompany }> = ({ company }) => {
-  const { airdrop } = company
-  if (!airdrop) {
-    return null
-  }
-  const unit = company.details.token.ticker
+const AirdropTokenDetails: React.FC<{ 
+    token: Token, 
+    airdrop: EnvoysAirdrop,
+  }> = ({ token, airdrop }) => {
+  const totalSupply = useTotalSupply(token);
 
   return (
     <>
       <TextWithHeader title="Token address">
-        <Link href="#" fontWeight={400} ellipsis={true} style={{ display: 'inline' }}>
-          {company.token}
+        <Link href={`https://bscscan.com/token/${token.address}`} external={true} fontWeight={400} ellipsis={true} style={{ display: 'inline' }}>
+          {token.address}
         </Link>
       </TextWithHeader>
 
       <TextWithHeader title="Airdrop address">
-        <Link href="#" fontWeight={400} ellipsis={true} style={{ display: 'inline' }}>
-          {airdrop.airdropAddress}
+        <Link href={`https://bscscan.com/address/${airdrop.address}`} external={true} fontWeight={400} ellipsis={true} style={{ display: 'inline' }}>
+          {airdrop.address}
         </Link>
       </TextWithHeader>
 
-      <TextWithHeader title="Name">{company.details.token.tickerName || unit}</TextWithHeader>
-      <TextWithHeader title="Symbol">{unit}</TextWithHeader>
+      <TextWithHeader title="Name">{token.name}</TextWithHeader>
+      <TextWithHeader title="Symbol">{token.symbol}</TextWithHeader>
       <TextWithHeader title="Total tokens">
-        {airdrop.totalTokens} {unit}
+        {totalSupply && totalSupply.toFixed(2)} {token.symbol}
       </TextWithHeader>
     </>
   )
